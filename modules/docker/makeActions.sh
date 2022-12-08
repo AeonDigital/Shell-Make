a983dbf32afad1ea4ae1ae1ea8a25aa232d22bfb
Rianna Cantarelli <rianna.aeon@gmail.com>
2022-12-07 23:01:10 -0300

    melhorando Help

--- modules/docker/makeActions.sh
+++ modules/docker/makeActions.sh
@@ -18,52 +18,6 @@
 
 
 #
-# Entra no bash do container principal do projeto
-#
-# Informe um parametro 'cont' para indicar em qual container deseja entrar.
-#   Valores aceitos são: web|db
-#   Se nenhum valor for informado, entrará no 'web'
-openContainerBash() {
-  if [ -z ${cont+x} ]; then
-    cont="web";
-  fi;
-
-  if [ "${cont}" == "web" ]; then
-    docker exec -it ${CONTAINER_WEBSERVER_NAME} /bin/bash;
-  elif [ "${cont}" == "db" ]; then
-    docker exec -it ${CONTAINER_DBSERVER_NAME} /bin/bash;
-  else
-    mse_inter_showAlert "f" "Parametro cont=\"${cont}\" inválido; use \"web\" ou \"db\"."
-  fi;
-}
-
-
-
-
-
-#
-# Retorna o IP da rede usado pelos containers
-getContainersIP() {
-  local tmpIP=""
-  local tmpMsgTitle="IP dos Containers"
-  declare -a arrMessage=()
-
-
-  if [ "${CONTAINER_WEBSERVER_NAME}" != "" ]; then
-    tmpIP=$(docker inspect ${CONTAINER_WEBSERVER_NAME} | grep -oP -m1 '(?<="IPAddress": ")[a-f0-9.:]+')
-    arrMessage+=("Web-Server : ${tmpIP}")
-  fi
-  if [ "${CONTAINER_DBSERVER_NAME}" != "" ]; then
-    tmpIP=$(docker inspect ${CONTAINER_DBSERVER_NAME} | grep -oP -m1 '(?<="IPAddress": ")[a-f0-9.:]+')
-    arrMessage+=("DB-Server  : ${tmpIP}")
-  fi
-
-  mse_inter_showAlert "a" "${tmpMsgTitle}" "arrMessage"
-}
-
-
-
-#
 # Verifica se o container alvo existe
 #
 # @param string $1
@@ -99,6 +53,78 @@ checkIfContainerExists() {
     else
       echo "1"
     fi
+  fi
+}
+
+
+
+
+
+#
+# Entra no bash do container principal do projeto
+#
+# Informe um parametro 'cont' para indicar em qual container deseja entrar.
+#   Opções: 'web' | 'db' | hash-ou-nome-do-container
+#   Se nenhum valor for informado, usará o valor 'web'
+openContainerBash() {
+  if [ -z ${cont+x} ]; then
+    cont="web";
+  fi;
+
+  if [ "${cont}" == "web" ]; then
+    docker exec -it ${CONTAINER_WEBSERVER_NAME} /bin/bash;
+  elif [ "${cont}" == "db" ]; then
+    docker exec -it ${CONTAINER_DBSERVER_NAME} /bin/bash;
+  else
+    docker exec -it ${cont} /bin/bash;
+  fi
+}
+
+
+
+#
+# Retorna o IP da rede usado pelos containers
+#
+# Informe um parametro 'cont' para indicar qual container deseja conferir o IP.
+#   Opções: 'web' | 'db' | hash-ou-nome-do-container
+#   Se nenhum valor for informado, usará o valor 'web'
+getContainersIP() {
+  local tmpIP=""
+  local tmpMsgTitle="IP dos Containers"
+  declare -a arrMessage=()
+
+
+  if [ "${CONTAINER_WEBSERVER_NAME}" != "" ]; then
+    tmpIP=$(docker inspect ${CONTAINER_WEBSERVER_NAME} | grep -oP -m1 '(?<="IPAddress": ")[a-f0-9.:]+')
+    arrMessage+=("Web-Server : ${tmpIP}")
+  fi
+  if [ "${CONTAINER_DBSERVER_NAME}" != "" ]; then
+    tmpIP=$(docker inspect ${CONTAINER_DBSERVER_NAME} | grep -oP -m1 '(?<="IPAddress": ")[a-f0-9.:]+')
+    arrMessage+=("DB-Server  : ${tmpIP}")
+  fi
+
+  mse_inter_showAlert "a" "${tmpMsgTitle}" "arrMessage"
+}
+
+
+
+#
+# Executa um 'tail' no log do container alvo.
+#
+# Informe um parametro 'cont' para indicar em qual container deseja se conectar.
+#   Opções: 'web' | 'db' | hash-ou-nome-do-container
+#   Se nenhum valor for informado, usará o valor 'web'
+logTail() {
+  if [ -z ${cont+x} ]; then
+    cont="web";
+  fi;
+
+  if [ "${cont}" == "web" ]; then
+    docker logs --follow ${CONTAINER_WEBSERVER_NAME}
+  elif [ "${cont}" == "db" ]; then
+    docker logs --follow ${CONTAINER_DBSERVER_NAME}
+  else
+    docker logs --follow ${cont}
   fi
 }
 
